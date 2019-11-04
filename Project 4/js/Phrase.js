e0c3d76418b2b7f6c05ff11c813d1f1afce2f05d
skrabacher <37253871+skrabacher@users.noreply.github.com>
2019-11-04 11:59:51 -0800

    addPhraseToDisplay drafted

--- Project 4/js/Phrase.js
+++ Project 4/js/Phrase.js
@@ -6,8 +6,33 @@
      constructor(phrase) {
         this.phrase = phrase.toLowerCase();//creates phrase property that converts input to lowercase
      }
+
+     addPhraseToDisplay() {
+    //   let space = document.createElement(li).addClass("space");
+    //   let iteratedLetter = "h";///pulled from for each function that will iterate through the phrase???
+    //   let letter = document.createElement(li).addClass("hide letter " + iteratedLetter).textContent = iteratedLetter;
+      return this.phrase.split(''); //selects the phrase and returns array consisting of each character in the phrase
+       
+
+     }
+     
  }
-
-
+///Make sure the phrase displayed on the screen uses the letter 
+// ///CSS class for letters and the space CSS class for spaces.
+// <div id="phrase" class="section">
+//     <ul>
+//         <li class="hide letter h">h</li>
+//         <li class="hide letter o">o</li>
+//         <li class="hide letter w">w</li>
+//         <li class="space"> </li>
+//         <li class="hide letter a">a</li>
+//         <li class="hide letter r">r</li>
+//         <li class="hide letter e">e</li>
+//         <li class="space"> </li>
+//         <li class="hide letter y">y</li>
+//         <li class="hide letter o">o</li>
+//         <li class="hide letter u">u</li>
+//     </ul>
+// </div>
 
 
--- Project 4/js/app.js
+++ Project 4/js/app.js
@@ -19,15 +19,17 @@
 
 //console.log(game.phrases[1]); //testing
 
-const logPhrase = (phrase) => {
-    console.log(`Phrase - phrase: `, phrase.phrase);
-    };
-    const game = new Game();
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
-    logPhrase(game.getRandomPhrase());
+// const logPhrase = (phrase) => {
+//     console.log(`Phrase - phrase: `, phrase.phrase);
+//     };
+//     const game = new Game();
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
+//     logPhrase(game.getRandomPhrase());
 
+const game = new Game();
+game.getRandomPhrase().addPhraseToDisplay();
 
 
