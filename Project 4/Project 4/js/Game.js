6da1157c90e9c79393c87ddc5c05b99282f625a7
skrabacher <37253871+skrabacher@users.noreply.github.com>
2019-11-05 14:40:42 -0800

    checkForWin method - DRAFT

---
+++ Project 4/Project 4/js/Game.js
@@ -0,0 +1,91 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Game.js */
+
+class Game { // creates game class
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+   
+    createPhrases() { // creates and returns an array of phrase objects
+        const phrasesArray = [
+            new Phrase("knock on wood"),
+            new Phrase("dime a dozen"),
+            new Phrase("easy as pie"),
+            new Phrase("break a leg"),
+            new Phrase("for the birds")
+        ];
+       return phrasesArray;
+
+    }
+    getRandomPhrase() { //generates random integer from 0-4 the uses random the integer to select the phrase indexed at that number in the array
+        const index = Math.floor(Math.random() * Math.floor(this.phrases.length)); // generates random integer from 0 to one less than the array length
+        return game.phrases[index]; //returns one random phrase as a string
+    }
+
+    startGame() { //hides the start screen, gets a phrase for the new game and displays gameboard for user
+        const startScreenOverlay = document.querySelector('#overlay');
+        startScreenOverlay.style.display = 'none'; //hides start screen overlay
+        let phraseObject = this.getRandomPhrase(); //selects a phrase object for the new game
+        console.log(phraseObject);
+        this.activePhrase = phraseObject; //assigns the selected phrase to the activePhrase property in the Game Class
+        this.activePhrase.addPhraseToDisplay(); //displays corresponding phrase gameboard
+        console.log(this.activePhrase);
+    }
+
+    checkForWin() { //4th try idea: use reduce method to convert matchedLettersLi to one giant string, then use the includes method on that string to determine if 'hide' is in the string. if hide is not in the string then the user has won
+        
+        //THIRD TRY
+        let matchedLettersLi = document.querySelectorAll('#phrase ul li');// selects LI's that represent each character in the phrase
+        matchedLettersLi.forEach(li => { //iterates through each LI node
+            console.log(li.className.toString()); //
+            let liClassString = li.className.toString(); //gets class value and converts to string so that includes method  can be used
+            console.log(liClassString);
+        if (liClassString.includes('hide') ) { //if LI node's class contains 'hide' then use has not won, return FALSE. else if there are no hide classes then user has won return TRUE
+            return false;
+        } else {
+            return true;
+        }
+        });
+        
+        //SECOND TRY
+        // let matchedLettersLi = document.querySelectorAll('#phrase ul li');// selects LI's that represent each character in the phrase
+        // matchedLettersLi = matchedLettersLi.values();
+        // console.log(matchedLettersLi);
+
+        // if (matchedLettersLi.includes('hide')) {
+        //     return false;
+        // } else {
+        //     return true;
+        // }
+
+        //FIRST TRY
+    //     let matchedLettersLi = document.querySelectorAll('#phrase ul li');// selects LI's that represent each character in the phrase
+    //     console.log(matchedLettersLi);
+    //     matchedLettersLi.forEach(li => { //iterates through each LI element
+    //     console.log(li.className);
+    //     if (li.className !== 'show' && li.className !== 'space') { //if LI's class name is not show or space, then user has not won
+    //       return false;
+    //     } else {
+    //         return true;
+    //     }
+    //   });
+     }
+    }
+
+//     `checkForWin()`: This method checks to see if the player has revealed all of the
+// letters in the active phrase. (return Boolean value)
+
+
+    // handleInteraction() {
+
+    // }
+
+ 
+
+
+
+ 
+
---
+++ Project 4/Project 4/js/Phrase.js
@@ -0,0 +1,51 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js */
+
+ class Phrase { //creates phrase class
+     constructor(phrase) {
+        this.phrase = phrase.toLowerCase();//creates phrase property that converts input to lowercase
+     }
+
+     addPhraseToDisplay() {
+      let phraseCharactersArray = this.phrase.split(''); //selects the phrase and returns array consisting of each character in the phrase
+      phraseCharactersArray.forEach(character => {
+          if (character === " ") { //if the character is a space
+            const li = document.createElement('li'); // creates LI node
+            li.className = 'space'; // adds space class to li node
+            li.textContent = ' '; //defines li text content
+            document.querySelector('#phrase ul').appendChild(li) // appends li to 
+
+          } else {
+             const li = document.createElement('li'); // creates LI node
+             li.className = `hide letter ${character}`; // adds letter class to li node
+             li.textContent = `${character}`; //defines li text content
+             document.querySelector('#phrase ul').appendChild(li) // appends li to 
+          }
+      });
+     }
+
+     checkLetter(letter) { //Checks to see if the letter selected by the player matches a letter in the phrase.
+      console.log("in the checkLetter method");
+      let phraseCharactersArray = this.phrase.split(''); //converts phrase to array
+      console.log(phraseCharactersArray);
+      phraseCharactersArray.forEach(character => {
+        if (character === letter) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+     }
+     
+     showMatchedLetter(letter) { //method receives the letter the user selected as a parameter and if the letter is in the phrase it will reveal the letter on the game board
+      let matchedLettersLi = document.querySelectorAll('#phrase ul li');// selects LI's that represent each character in the phrase
+      matchedLettersLi.forEach(li => { //iterates through each LI element
+        if (li.innerHTML === letter) { //if LI element's innerHTML is the same as the letter the user selected
+          li.className = 'show'; //show the LI on the page(by changing it's css class)
+        }
+      });
+     }
+ }
+
+
